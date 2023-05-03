db60efd8c2bd773f4a0ccc2412c925d8a9119db3
alexpetrovan <alex.petrovan@stud.ubbcluj.ro>
2023-05-03 19:09:51 +0300

    URL updated with the secure dns

--- reactFrontend/src/constants.ts
+++ reactFrontend/src/constants.ts
@@ -1,3 +1,1 @@
-export const BACKEND_API_URL = "http://13.49.102.193/api";
-// /api/* http://13.49.102.193:443/api/:splat 200
-// /* /index.html 200
+export const BACKEND_API_URL = "https://movieapi.jumpingcrab.com/api";
