b58a38f254b28f9b23f6c19414206903c18dafd7
alexpetrovan <alex.petrovan@stud.ubbcluj.ro>
2023-05-17 23:04:25 +0300

    Update generate_productions.py

--- backend/populate_scripts/generate_productions.py
+++ backend/populate_scripts/generate_productions.py
@@ -39,7 +39,7 @@ for i in range(num_productions // batch_size):
         # Generate other production attributes (origin_country, website, description)
 
         origin_country = fake.country()
-        website = fake.unique.url()
+        website = "www"+company_name+".com"
         description = fake.text(max_nb_chars=50)
 
         # Generate the added_by field
