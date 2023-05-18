2907117317477147a69967b4669d588ed2e09b73
alexpetrovan <alex.petrovan@stud.ubbcluj.ro>
2023-05-18 12:08:46 +0300

    lab 8-9 done

--- reactFrontend/src/components/actors/ActorsShowAll.tsx
+++ reactFrontend/src/components/actors/ActorsShowAll.tsx
@@ -1,10 +1,11 @@
 import { useEffect, useState} from "react";
 import { Actor } from "../../models/Actor";
-import { Container, CircularProgress, Paper, Table, TableCell, TableContainer, TableHead, TableRow, TableBody, Typography, Menu, FormControl, InputLabel, Select, MenuItem, SelectChangeEvent } from "@mui/material";
+import { Container, CircularProgress, Paper, Table, TableCell, TableContainer, TableHead, TableRow, TableBody, Typography, Menu, FormControl, InputLabel, Select, MenuItem, SelectChangeEvent, Checkbox } from "@mui/material";
 import { BACKEND_API_URL } from "../../constants";
 
 import { PaginationComponent } from "../../customPagination/pagination";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 
 export const AllActors = () => {
@@ -13,13 +14,17 @@ export const AllActors = () => {
     const [actorCount, setActorCount] = useState(0);
     const [currentPage, setPage] = useState(1);
     const [pageSize, setPageSize] = useState<number>(100);
+    const [idsToDelete, setIdsToDelete] = useState<number[]>([]);
+    const [fetchData, setFetchData] = useState(true);
+
+    const isAdmin = localStorage.getItem("role") == "admin";
 
     useEffect(() => {
         setLoading(true);
         const size = localStorage.getItem("pageSize");
         if (size !== null) {
             setPageSize(parseInt(size, 10));
-            fetch(`${BACKEND_API_URL}/Actor/?page_size=${size}`)
+            fetch(`${BACKEND_API_URL}/Actor/?page_size=${size}&page=${currentPage}`)
                 .then((response) => response.json())
                 .then((data) => {
                     setActors(data.results);
@@ -35,7 +40,34 @@ export const AllActors = () => {
                     setLoading(false);
                 });
         }
-    }, []);
+    }, [fetchData]);
+
+    function handleCheckBoxChanged(event: React.ChangeEvent<HTMLInputElement>, id: number): void {
+        if (id === 0)
+            return;
+        if (event.target.checked === true) {
+            setIdsToDelete(prevIds => [...prevIds, id]);
+        }
+        else { setIdsToDelete(prevIds => prevIds.filter(prevId => prevId !== id)); }
+    }
+
+    const isSomethingChecked = idsToDelete.length > 0;
+
+    const handleDeleteAll = async () => {
+
+        try {
+            const token = localStorage.getItem("token");
+            if (token !== null) {
+                const headers = { Authorization: `Bearer ${token}`};
+                await axios.delete(`${BACKEND_API_URL}/Delete/production/`, { data: {ids: idsToDelete}, headers: headers, });
+                setIdsToDelete([]);
+                setFetchData((prevValue) => !prevValue);
+            }
+        } catch (error) {
+            throw new Error("Error deleting productions");
+        }
+
+    }
 
     return (
         <Container>
@@ -51,7 +83,7 @@ export const AllActors = () => {
                     <Table aria-label="movie-table">
                         <TableHead>
                             <TableRow>
-                                <TableCell>#</TableCell>
+                                {isAdmin ? (<TableCell>Delete</TableCell>) : (<TableCell>#</TableCell>)}
                                 <TableCell align="right">Name</TableCell>
                                 <TableCell align="right">Gender</TableCell>
                                 <TableCell align="right">Age</TableCell>
@@ -66,9 +98,14 @@ export const AllActors = () => {
                                 actors
                                     .map((actor, index) => (
                                         <TableRow key={actor.id}>
-                                            <TableCell component="th" scope="row">
-                                                {index + 1 + (currentPage-1)*pageSize}
-                                            </TableCell>
+                                            {isAdmin ? (<TableCell>
+                                                <Checkbox
+                                                    onChange={(event) => handleCheckBoxChanged(event, actor?.id || 0)}
+                                                />
+                                            </TableCell>) :
+                                                (<TableCell component="th" scope="row">
+                                                    {index + 1 + (currentPage - 1) * pageSize}
+                                                </TableCell>)}
                                             <TableCell align="right">{actor.name}</TableCell>
                                             <TableCell align="right">{actor.gender}</TableCell>
                                             <TableCell align="right">{actor.age}</TableCell>
